92f50337a23da804322bfbe828faf6c72ee01237
farnamah <145441216+farnamah@users.noreply.github.com>
2023-11-26 17:56:55 +0330

    Models for apps(blog and account)
    making custom user for apps in models

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """
+    custom user model manager where email is the unique identifiers for authenticated instead of usernames
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        create and save a user with the given email and password and extra data
+        """
+        pass
+        if not email:
+            raise ValueError("the email must not be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        create and save superuser with given email and password
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user Model for app
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=20)
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.email
